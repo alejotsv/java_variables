d8c270ef0f359b3ce9ca77ff7972317a1f86b0b5
Alejandro Salas <asalas.wd@outlook.com>
2022-10-06 22:39:37 -0400

    Add variables

--- src/com/java21days/Variables.java
+++ src/com/java21days/Variables.java
@@ -1,4 +1,11 @@
 package com.java21days;
 
 public class Variables {
+    public static void main(String[] args) {
+        final char UP = 'U';
+        byte initialLevel = 12;
+        short location = 13250;
+        int score = 3500100;
+        boolean newGame = true;
+    }
 }
